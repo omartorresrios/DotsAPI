e49c3c18e6a29d3791009838749fc908b3feb67f
Omar Torres <torresomar44@gmail.com>
2017-05-05 16:31:06 -0500

    Allow user to signin with either email or username

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ActiveRecord::Base
                     format: { with: EMAIL_REGEX }
   validates :username, presence: true, uniqueness: { case_sensitive: false }
   validates :fullname, presence: true
+
+  def self.authenticate(email_or_username, password)
+    user = User.find_by(email: email_or_username) || User.find_by(username: email_or_username)
+    user && user.authenticate(password)
+  end
 end
