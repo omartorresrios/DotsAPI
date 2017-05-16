3063fb34d09782e1282249019f211046231069b4
Omar Torres <torresomar44@gmail.com>
2017-05-16 16:25:15 -0500

    Minor changes in signup and signin features

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,12 +7,51 @@ class Users::RegistrationsController < ApplicationController
   #   password: String
   # }
   def create
-    user = User.new(user_params)
-    if user.save
-      render json: user, serializer: CurrentUserSerializer, status: 201
-    else
-      render json: { errors: user.errors.full_messages }, status: 422
+    if request.post?
+      if params && params[:full_name] && params[:email] && params[:password]
+        
+        params[:user] = Hash.new    
+        params[:user][:first_name] = params[:full_name].split(" ").first
+        params[:user][:last_name] = params[:full_name].split(" ").last
+        params[:user][:email] = params[:email]
+        
+        begin 
+          decrypted_pass = AESCrypt.decrypt(params[:password], ENV["API_AUTH_PASSWORD"])
+        rescue Exception => e
+          decrypted_pass = nil          
+        end
+                
+        params[:user][:password] = decrypted_pass  
+        params[:user][:verification_code] = rand_string(20)
+        
+        user = User.new(user_params)
+
+        if user.save
+          render json: user, serializer: CurrentUserSerializer, status: 201
+          # render :json => user.to_json, :status => 200
+        else
+          render json: { errors: user.errors.full_messages }, status: 422
+          # error_str = ""
+
+          # user.errors.each{|attr, msg|           
+          #   error_str += "#{attr} - #{msg},"
+          # }
+                    
+          # e = Error.new(:status => 400, :message => error_str)
+          # render :json => e.to_json, :status => 400
+        end
+      else
+        e = Error.new(:status => 400, :message => "required parameters are missing")
+        render :json => e.to_json, :status => 400
+      end
     end
+
+    # user = User.new(user_params)
+    # if user.save
+    #   render json: user, serializer: CurrentUserSerializer, status: 201
+    # else
+    #   render json: { errors: user.errors.full_messages }, status: 422
+    # end
   end
 
   private
