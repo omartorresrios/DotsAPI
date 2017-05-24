e625661da17b0a3deaa48330e80d5534099bd78f
Omar Torres <torresomar44@gmail.com>
2017-05-23 23:07:20 -0500

    delete anonymous as parameter in ProfileController write method

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -28,7 +28,7 @@ class ProfileController < ApplicationController
 
   private
     def review_params
-      params.require(:review).permit(:content, :anonymous)
+      params.require(:review).permit(:content)
     end
 
     def set_user
