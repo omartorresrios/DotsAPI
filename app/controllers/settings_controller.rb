6eb5418bf2c9e5c7ee661aaf9dca4f44513873e2
Omar Torres <torresomar44@gmail.com>
2017-05-31 15:59:50 -0500

    Uncomment authenticate_user from SettingsController

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -13,17 +13,8 @@ class SettingsController < ApplicationController
 
   end
 
-  # def update_avatar
-  #   current_user.update params.require(:user).permit(:avatar)
-  # end
-
   def update_avatar
-    image = StringIO.new(Base64.decode64(params[:avatar].tr(' ', '+')))
-    image.class.class_eval { attr_accessor :original_filename, :content_type }
-    image.original_filename = SecureRandom.hex + '.png'
-    image.content_type = 'image/png'
-
-    current_user.update_attributes(image)
+    current_user.update params.require(:user).permit(:avatar)
   end
 
   protected
