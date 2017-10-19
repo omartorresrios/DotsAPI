4625fad193c1c945acea5b41882dd8ca8c6ebb3e
Omar Torres <torresomar44@gmail.com>
2017-10-19 14:41:52 -0500

    Little change

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -3,7 +3,11 @@ class EventController < ApplicationController
 
   def index
     events = Event.all
-    render json: events.to_json(:methods => [event_url]), status: 200
+    # render json: events.to_json(:methods => [event_url]), status: 200
+    render :json => events.to_json(:only => [:id, :created_at, :description, :likes_count, :picture_content_type, 
+         :picture_file_name, :picture_file_size, :picture_updated_at,
+         :updated_at, :user_id, :video_content_type, :video_file_name,
+         :video_file_size, :video_updated_at], :methods => [:avatar_url]), status: 200
   end
 
   def create
--- app/serializers/all_events_serializer.rb
+++ app/serializers/all_events_serializer.rb
@@ -2,7 +2,7 @@ class AllEventsSerializer < ActiveModel::Serializer
   attributes :id, :created_at, :description, :likes_count, :picture_content_type, 
   			 :picture_file_name, :picture_file_size, :picture_updated_at,
   			 :updated_at, :user_id, :video_content_type, :video_file_name,
-  			 :video_file_size, :video_updated_at, :event_url
+  			 :video_file_size, :video_updated_at
 
   belongs_to :user, serializer: UserSerializer
 end
