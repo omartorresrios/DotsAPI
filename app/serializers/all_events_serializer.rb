8950b8fdb2505f39f38274ae9e6913ac23a50c86
Omar Torres <torresomar44@gmail.com>
2017-10-18 22:10:30 -0500

    Add belongs to in AllEventsSerializer with user

--- app/serializers/all_events_serializer.rb
+++ app/serializers/all_events_serializer.rb
@@ -3,4 +3,6 @@ class AllEventsSerializer < ActiveModel::Serializer
   			 :picture_file_name, :picture_file_size, :picture_updated_at,
   			 :updated_at, :user_id, :video_content_type, :video_file_name,
   			 :video_file_size, :video_updated_at, :event_url
+
+  belongs_to :user, serializer: UserSerializer
 end
