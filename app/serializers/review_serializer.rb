c9cc337b3b1882da480725afc39dcf915214fca6
Omar Torres <torresomar44@gmail.com>
2017-12-16 18:15:41 -0500

    get rid of review_url from ReviewSerializer

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,11 +1,7 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :audio, :duration, :review_url, :created_at, :from, :to
+  attributes :id, :audio, :duration, :created_at, :from, :to
 
   belongs_to :sender, :class_name => 'User', :foreign_key => 'from', serializer: UserSerializer
   belongs_to :receiver, :class_name => 'User', :foreign_key => 'to', serializer: UserSerializer
-
-  def review_url
-    object.audio.url
-  end
 
 end
