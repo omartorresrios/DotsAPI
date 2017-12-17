0e8df7f5830c673a77797319c7e8bc5c99076c61
Omar Torres <torresomar44@gmail.com>
2017-12-16 19:29:28 -0500

    get rid of belongs in reviewserializer

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,7 +1,7 @@
 class ReviewSerializer < ActiveModel::Serializer
   attributes :id, :audio, :duration, :created_at, :from, :to
 
-  belongs_to :sender, :class_name => 'User', :foreign_key => 'from', serializer: UserSerializer
-  belongs_to :receiver, :class_name => 'User', :foreign_key => 'to', serializer: UserSerializer
+  # belongs_to :sender, :class_name => 'User', :foreign_key => 'from', serializer: UserSerializer
+  # belongs_to :receiver, :class_name => 'User', :foreign_key => 'to', serializer: UserSerializer
 
 end
