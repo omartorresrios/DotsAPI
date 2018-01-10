12de34923d43184e573c9d4c30196479a68dd443
Omar Torres <torresomar44@gmail.com>
2018-01-10 14:16:31 -0500

    Add duration column to event model

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -19,6 +19,6 @@ class EventController < ApplicationController
   private
 
     def event_params
-      params.permit(:description, :video)
+      params.permit(:description, :video, :duration)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171216195649) do
+ActiveRecord::Schema.define(version: 20180110190904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20171216195649) do
     t.string   "video_content_type"
     t.integer  "video_file_size"
     t.datetime "video_updated_at"
+    t.string   "duration"
   end
 
   create_table "reviews", force: :cascade do |t|
