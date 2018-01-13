f7e186e7814d908eaf7f6d521009f352385ea364
Omar Torres <torresomar44@gmail.com>
2018-01-13 11:55:02 -0500

    Get rid of description presence true in event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,7 +7,6 @@ class Event < ActiveRecord::Base
   # has_many :tags, through: :taggings
 
   validates :user_id, presence: true
-  validates :description, presence: true
 
   # has_attached_file :picture, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150>" }
   # validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
