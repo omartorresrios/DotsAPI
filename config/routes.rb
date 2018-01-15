8c7c7da004e323a34637c9acd090619941fc787b
Omar Torres <torresomar44@gmail.com>
2018-01-14 21:40:01 -0500

    retrieve user events by email

--- config/routes.rb
+++ config/routes.rb
@@ -41,7 +41,7 @@ Rails.application.routes.draw do
     # Public profile (other users)
     get ':fullname/public_profile' => 'profile#show'
     post ':fullname/speak' => 'profile#speak'
-    get ':fullname/events' => 'profile#events'
+    get ':email/events' => 'profile#events'
     get ':fullname/reviews' => 'profile#reviews'
 
     get 'all_users' => 'search#index'
